8931fa6e4c87c0a71d94a800b55eecb11822ac74
cneumann01 <124218362+cneumann01@users.noreply.github.com>
2025-05-29 23:43:25 -0400

    Basic Sensor/Collision Detection

--- main.js
+++ main.js
@@ -2,23 +2,22 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const road=new Road(canvas.width/2,canvas.width*.9)
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-car.draw(ctx);
 
 animate();
 
-function animate(){
-    car.update();
+function animate() {
+	canvas.height = window.innerHeight;
 
-    canvas.height = window.innerHeight;
+	car.update(road.borders);
 
-    ctx.save();
-    ctx.translate(0,-car.y+canvas.height*0.8);
+	ctx.save();
+	ctx.translate(0, -car.y + canvas.height * 0.8);
 
-    road.draw(ctx)
-    car.draw(ctx);
+	road.draw(ctx);
+	car.draw(ctx);
 
-    ctx.restore();
-    requestAnimationFrame(animate)
+	ctx.restore();
+	requestAnimationFrame(animate);
 }
